66283f1336f60fed02cb09c7f0b367f74c4b60b7
jimlitical <jimlitical@gmail.com>
2016-05-21 18:55:56 -0700

    Script was missing on index. Got the scroll to work for the two buttons.

--- MDB/js/scrolltop.js
+++ MDB/js/scrolltop.js
@@ -2,20 +2,17 @@
 
 $(document).ready(function(){
     $(".nav-link").click(function(){
-        console.log($(this).attr("name"));
         var name = $(this).attr("name");
-        console.log($('#in-seattle').offset());
         if(name === 'inseattle')
         {
             $('html, body').animate({
                 scrollTop: $('#in-seattle').offset().top-90
             }, 1000);
         }
-        
-        if(name === 'inseattle')
+        else if(name === 'oncampus')
         {
             $('html, body').animate({
-                scrollTop: $('#in-seattle').offset().top-90
+                scrollTop: $('#on-campus').offset().top-90
             }, 1000);
         }
   });
